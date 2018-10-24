7863b476f4e877a578f9f82d7c967b1af55becab
Lesechka <littlecare@mail.ru>
2018-10-24 06:41:54 +0300

    Edited solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 module.exports = function solveEquation(equation) {
-var numbers = equation.match(/(-?|\+\s|-\s)[^\^][0-9]+/g).map(function(item){
+let numbers = equation.match(/(-?|\+\s|-\s)[^\^][0-9]+/g).map(function(item){
   return Number(item.replace(/\s/g, ''));
 });
-var [a,b,c] = numbers;
-var D = Math.pow(b, 2) - 4 * a * c;
-    var first = (-b + Math.sqrt(D)) / (2 * a);
-    var second = (-b - Math.sqrt(D)) / (2 * a);
-          var result = [Math.round(first), Math.round(second)];
+const [a,b,c] = numbers;
+const D = Math.pow(b, 2) - 4 * a * c;
+    const first = (-b + Math.sqrt(D)) / (2 * a);
+    const second = (-b - Math.sqrt(D)) / (2 * a);
+          const result = [Math.round(first), Math.round(second)];
           return result.sort(function(a,b) {
             return a-b;
           });
